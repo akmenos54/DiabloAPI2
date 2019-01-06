4d82321415da6cb385e6221dae85a1c0df5d570a
KLEIN Nicolas <kleinnico54@gmail.com>
2019-01-06 12:26:35 +0100

    fix

--- src/test/java/diabloapi/DemoApplicationTests.java
+++ src/test/java/diabloapi/DemoApplicationTests.java
@@ -1,9 +1,24 @@
 package diabloapi;
 
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -13,11 +28,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoApplicationTests {
 
-	/**
-	 * Context loads.
-	 */
-	@Test
-	public void contextLoads() {
-	}
+	
 
 }
